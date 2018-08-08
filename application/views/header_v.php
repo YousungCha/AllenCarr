badca92dc361ee1e3612b30016534d8a16278f5f
Yousung Cha <yscha08@gmail.com>
2018-08-08 13:32:04 +0900

    add : meta tag

--- application/views/header_v.php
+++ application/views/header_v.php
@@ -4,7 +4,7 @@
 	<head>
 		<title>알렌카 - 세계 최고 효율의 금연법</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=0">		
-		<meta property="og:title" content="알렌카 - 금연 역사를 다시쓰다." />
+		<meta property="og:title" content="당신의 금연을 다시 쓰다." />
 		<meta property="og:type" content="website" />
 		<meta property="og:url" content="http://www.allencarr.co.kr" />
 		<meta property="og:image" content="http://www.allencarr.co.kr/images/allencarr_sns.png"/>
