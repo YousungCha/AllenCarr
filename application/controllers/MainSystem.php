83643fd62e17507c239e6cea9647eca342c69fd4
Yousung Cha <yscha08@gmail.com>
2018-08-09 22:48:36 +0900

    modify : appropriate to ssl

--- application/controllers/MainSystem.php
+++ application/controllers/MainSystem.php
@@ -9,27 +9,12 @@ class MainSystem extends CI_Controller
 		$this->load->view('menu_v');
 		$this->load->view('hero_v');
 		$this->load->view('notice_v');
-		//$this->load->view('value_v');
-		//$this->load->view('method_v');
-		//$this->load->view('easyway_v');
-		//$this->load->view('member_v');
-		//$this->load->view('partner_v');
-		//$this->load->view('testi_v');
-		$this->load->view('footer_v');
-	}
-	public function vMain()
-	{
-		$this->load->view('header_v');		
-		$this->load->view('menu_v');
-		$this->load->view('hero_v');
-		$this->load->view('notice_v');
 		$this->load->view('value_v');
 		$this->load->view('method_v');
 		$this->load->view('easyway_v');
 		$this->load->view('member_v');
 		$this->load->view('partner_v');
 		$this->load->view('testi_v');
-		$this->load->view('faq_v');
 		$this->load->view('footer_v');
 	}
 }
