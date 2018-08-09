8a87cc93f602f0550b5930c13ac35e2cf2877e73
Yousung Cha <yscha08@gmail.com>
2018-08-09 14:19:33 +0900

    add : start to mobile responsive

--- application/views/footer_v.php
+++ application/views/footer_v.php
@@ -1,15 +1,15 @@
 	<div class="continer-fluid bacred2 set-mgn">
-		<div class="h070"></div>
+		<div class="h060"></div>
 		<center>
 		<div class="row def-width-1200">
 			<div class="col-md-6 tale">
-				<a href="#" onclick="moveTop();"><img src="/images/logo.png" style="width:200px;"></a>
+				<a href="#" onclick="moveTop();"><img src="/images/logo.png" style="width:150px;"></a>
 				<div class="h030"></div>
-				<p class="cp3 twhite we200 lt000 ln15 tale">
+				<p class="cp3 twhite we200 lt000 ln18 tale">
 					(주)와이에이치 I 대표이사 : 차유성 I 경기도 군포시 산본로 378 산본사이버텔 907호<br>
 					사업자등록번호 : 123-86-36014 I 통신판매업신고 : 0214-경기군포-0073 I 개인정보책임자 : 김창환<br>
 				</p>	
-				<div class="h015"></div>
+				<div class="h010"></div>
 				<p class="cp3 twhite we500 lt000 ln18">Copyright ©2018 Allen Carr's Easyway (International) Ltd
 	All rights reserved</p>
 			</div>
--- application/views/hero_v.php
+++ application/views/hero_v.php
@@ -17,7 +17,7 @@
 		<img src="/images/trailer.jpg" style="width:300px; border:10px solid #E7E7E7;">
 		<div class="h080"></div>
 
-		<p class="cp2 we700 tgray1 lt000 ln18">
+		<p class="sns-menu-desk cp2 we700 tgray1 lt000 ln20">
 			<img src="/images/icon-kakao.png" style="margin-right: 7px; margin-top:-2px;"> 카카오톡 공식 채널
 			<img src="/images/icon-kakao.png" style="margin-left: 20px; margin-right: 7px; margin-top:-2px;"> 네이버 카페
 			<img src="/images/icon-blog.png" style="width:25px; margin-left: 20px; margin-right: 7px;"> 네이버 블로그
