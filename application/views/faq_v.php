f23644d654661f087c9290cec3f12fab0f742488
Yousung Cha <yscha08@gmail.com>
2018-08-22 08:45:20 +0900

    add : faq contents

--- application/views/faq_v.php
+++ application/views/faq_v.php
@@ -56,7 +56,7 @@
 							<div id="faq-3" class="cp1 we300 ln18 tale pdglr30 tblack" style="display: none;">
 								<div class="h015"></div>
 								모든 면에서 180도 달리 접근합니다.<br><br>
-								<b>1.</b> 가장 큰 차이점은 '의지력'을 주문하지 않는다는 점입니다. 그 어떤 경우에도 담배를 억지로 참고 버티라고 말하지 않습니다.<br><br>
+								<b>1.</b> <font class="tacred">가장 큰 차이점은 '의지력'을 주문하지 않는다는 점</font>입니다. 그 어떤 경우에도 담배를 억지로 참고 버티라고 말하지 않습니다.<br><br>
 								<b>2.</b> 약물, 대체품, 껌, 패치, 보조제 등 그 어떤 것도 절대 권하지 않습니다. 오히려 이런 것들은 금연에 방해가 된다고 말합니다.<br><br>
 								<b>3.</b> 그 어떤 경우에도 금단증상이 존재하지 않습니다. 흡연자들이 특히 믿기 어려워하는 부분이지만 이는 사실입니다. 참가자들의 후기에서도 가장 두드러지는 부분입니다.<br><br>
 								<b>4.</b> 담배를 끊은 당일부터 술자리에 참여하라고 말합니다. 당구도 치고, 맵고 기름진 음식도 먹으며, 흡연자 친구들도 만나라고 말합니다. 이런 것들을 포기하고는 금연이 즐거울 수 없습니다.<br><br>
@@ -164,36 +164,91 @@
 								<div class="h015"></div>
 							</div>
 						</td>
-					</tr>					
+					</tr>
 					<tr style="height: 20px;"></tr>
-					<tr>
-						<td style="width: 25px;"><font class="dp2 we700 tacred">+</font></td>
+					<tr style="cursor: pointer;" onclick="openSection('faq-9');">
+						<td style="width: 25px;"><font class="dp2 we700 tacred plus_faq-9">+</font></td>
 						<td class="cp1 we700 ln15 tale">왜 같은 내용을 듣고도 사람마다 결과가 다른가요?</td>
 					</tr>
-					<tr style="height: 20px;"></tr>
 					<tr>
-						<td style="width: 25px;"><font class="dp2 we700 tacred">+</font></td>
+						<td colspan="2">
+							<div id="faq-9" class="cp1 we300 ln18 tale pdglr30 tblack" style="display: none;">
+								<div class="h015"></div>
+								대상이 기계가 아니라 '사람'이기 때문입니다.<br><br>
+								알렌카는 사람의 사고를 변화합니다. 사고의 변화는 결코 강제적으로 이루어질 수 없습니다. 모든 것은 설득과 이해, 그리고 수용의 과정을 거쳐야 합니다.<br><br>
+								잘 아시다시피, 흡연자는 기본적으로 의심이 많습니다. 또한 담배를 피우는데 처한 상황, 흡연 방식, 담배에 대한 사고 방식까지 너무나도 다양합니다.<br><br>
+								알렌카는 다수 흡연자의 사고 방식부터 소수 흡연자의 독특한 사고까지 포괄적으로 다룹니다. 그 모든 내용이 알렌카의 금연테라피 1, 2, 3차에 녹아있는 것입니다. 모든 차수에 참여했을 때를 기준으로 대다수가 금연에 성공하는 이유이기도 합니다.
+							</div>
+						</td>
+					</tr>					
+					<tr style="height: 20px;"></tr>
+					<tr style="cursor: pointer;" onclick="openSection('faq-10');">
+						<td style="width: 25px;"><font class="dp2 we700 tacred plus_faq-10">+</font></td>
 						<td class="cp1 we700 ln15 tale">실패하면 정말 환불해주나요? 절차는 어떻게 되나요?</td>
 					</tr>
+					<tr>
+						<td colspan="2">
+							<div id="faq-10" class="cp1 we300 ln18 tale pdglr30 tblack" style="display: none;">
+								<div class="h015"></div>
+								환불보증제도는 알렌카 런던 본사를 비롯한 세계 모든 곳에서 국제표준으로 운영되고 있습니다.<br><br>
+								알렌카의 환불보증제도는 무척 단순하고 알기 쉽습니다. 다음의 조건을 만족할 경우 금연 성공/실패 여부와 무관하게 환불을 보장합니다.<br><br>
+								<p class="cp2 we300 tacred ln18">
+									<b>1.</b> 보증기한은 1차 참여일로부터 3개월입니다.<br>
+									<b>2.</b> 1차 참여일로부터 3개월 이내에 2차, 3차를 1회씩 참여해야 합니다.<br>
+									<b>3.</b> 15분 이상 지각, 당일 취소, 무단 불참이 없어야 합니다.<br>
+								</p>
+								담배를 피웠는지 아닌지 여부를 따지지 않습니다. 참여한 흡연자의 말씀을 전적으로 신뢰하고 겸허히 수용합니다.<br><br>
+							</div>
+						</td>
+					</tr>					
 					<tr style="height: 20px;"></tr>
 				</table>
 			</div>			
 			<div class="col-md-6 pdg00">
 				<table width="100%">
-					<tr>
-						<td style="width: 25px;"><font class="dp2 we700 tacred">+</font></td>
+					<tr style="cursor: pointer;" onclick="openSection('faq-11');">
+						<td style="width: 25px;"><font class="dp2 we700 tacred plus_faq-10">+</font></td>
 						<td class="cp1 we700 ln15 tale">자신있다면 3개월이 아니라 평생 환불보증을 해야하지 않나요?</td>
 					</tr>
-					<tr style="height: 20px;"></tr>
 					<tr>
-						<td style="width: 25px;"><font class="dp2 we700 tacred">+</font></td>
+						<td colspan="2">
+							<div id="faq-11" class="cp1 we300 ln18 tale pdglr30 tblack" style="display: none;">
+								<div class="h015"></div>
+								대다수 흡연자는 선량하고 합리적이지만, 그렇지 않은 사람들도 분명히 있습니다.<br><br>
+								알렌카에서 평생의 해방감이라는 놀라운 느낌을 받은 분들은 대다수 고마운 마음을 전합니다. 그러나 사람의 마음은 시간이 지남에 따라 바뀔 수 있습니다. 그렇게 고마워 했던 분들도 본의 아니게 삶이 힘들어지면 알렌카에 돈을 맡겨 놓은 것처럼 찾아갈 수 있겠지요.<br><br>
+								참여해본 분들은 알고 있습니다. 3개월이라는 기간은 본인의 금연 성공 여부를 판단하기에 충분한 시간이라는 것을 말이지요. 3개월 안에 평생 금연에 성공할 수 있다는 자신감이 생기지 않는다면, 환불을 보증하는 것입니다.<br><br>
+							</div>
+						</td>
+					</tr>						
+					<tr style="height: 20px;"></tr>
+					<tr style="cursor: pointer;" onclick="openSection('faq-12');">
+						<td style="width: 25px;"><font class="dp2 we700 tacred plus_faq-12">+</font></td>
 						<td class="cp1 we700 ln15 tale">3개월이 지나면 재참여가 불가능한가요?</td>
 					</tr>
-					<tr style="height: 20px;"></tr>
 					<tr>
-						<td style="width: 25px;"><font class="dp2 we700 tacred">+</font></td>
+						<td colspan="2">
+							<div id="faq-12" class="cp1 we300 ln18 tale pdglr30 tblack" style="display: none;">
+								<div class="h015"></div>
+								대다수 흡연자는 1차, 2차, 3차를 참여하는 동안 행복한 비흡연자가 됩니다. 그러나 실패 이후에도 환불보다는 재참여를 원하는 분들이 많습니다. <br><br>
+								이런 경우 심화테라피(2차, 3차)는 1년 동안 비용에 제한없이 참여할 수 있으며, 1차의 경우 담당 테라피스트의 판단 하에 반드시 필요할 경우 참여가 가능합니다.
+							</div>
+						</td>
+					</tr>
+					<tr style="height: 20px;"></tr>
+					<tr style="cursor: pointer;" onclick="openSection('faq-13');">
+						<td style="width: 25px;"><font class="dp2 we700 tacred plus_faq-13">+</font></td>
 						<td class="cp1 we700 ln15 tale">1년 뒤 다시 담배를 피우면, 처음부터 돈을 다시 내야 하나요?</td>
 					</tr>
+					<tr>
+						<td colspan="2">
+							<div id="faq-13" class="cp1 we300 ln18 tale pdglr30 tblack" style="display: none;">
+								<div class="h015"></div>
+								금연테라피 1차 참여일로부터 1년이 지났다면 심화테라피(2차, 3차)보다는 1차에 다시 참여하는 것을 강력하게 권해드립니다. 심화테라피는 1차 테라피의 기본이 되어 있어야 제대로 작동하기 때문입니다.<br><br>
+								<font class="tacred">
+								이 경우에는 원래 지불하셨던 비용의 50%의 금액으로 재참여가 가능하며, 모든 환불보증제도는 재참여 일자로부터 같은 조건으로 다시 시작합니다.</font>
+							</div>
+						</td>
+					</tr>					
 					<tr style="height: 20px;"></tr>
 				</table>				
 			</div>
@@ -204,48 +259,129 @@
 			<div class="h020"></div>
 			<div class="col-md-6 pdg00">
 				<table width="100%">
-					<tr>
-						<td style="width: 25px;"><font class="dp2 we700 tacred">+</font></td>
+					<tr style="cursor: pointer;" onclick="openSection('faq-14');">
+						<td style="width: 25px;"><font class="dp2 we700 tacred plus_faq-14 plus_faq-14">+</font></td>
 						<td class="cp1 we700 ln15 tale">금연에 성공했는지, 실패했는지 어떻게 알 수 있나요?</td>
 					</tr>
-					<tr style="height: 20px;"></tr>
 					<tr>
-						<td style="width: 25px;"><font class="dp2 we700 tacred">+</font></td>
+						<td colspan="2">
+							<div id="faq-14" class="cp1 we300 ln18 tale pdglr30 tblack" style="display: none;">
+								<div class="h015"></div>
+								알렌카는 다음과 같은 행복한 비흡연자의 확신을 선물로 드립니다.<br><br>
+								남은 평생 피우지 않을 수 있는 자신감<br>
+								안 피워도 전혀 괴롭지도, 우울하지도 않은 느낌<br>
+								드디어 해방되어 너무나도 행복하다는 느낌<br>
+								담배는 이렇게 끊는 것이다라는 알렌카에 대한 동의<br><br>
+								위와 같은 느낌과 반대로 지속적인 우울감, 괴로움, 자신감 결여 등이 이어진다면 이것은 실패로 보셔도 좋습니다. <font class="tacred">담배를 피우는지 아닌지의 여부보다 흡연자 본인이 괴롭다면 실패</font>입니다.
+							</div>
+						</td>
+					</tr>
+					<tr style="height: 20px;"></tr>
+					<tr style="cursor: pointer;" onclick="openSection('faq-15');">
+						<td style="width: 25px;"><font class="dp2 we700 tacred plus_faq-15">+</font></td>
 						<td class="cp1 we700 ln15 tale">매우 내성적인 성격입니다. 혹시 사람들 사이에서 말을 해야하나요?</td>
 					</tr>
-					<tr style="height: 20px;"></tr>
 					<tr>
-						<td style="width: 25px;"><font class="dp2 we700 tacred">+</font></td>
-						<td class="cp1 we700 ln15 tale">보조제로 아무 효과가 없었어요. 중독을 끊는 것이 정말 가능할까요?</td>
+						<td colspan="2">
+							<div id="faq-15" class="cp1 we300 ln18 tale pdglr30 tblack" style="display: none;">
+								<div class="h015"></div>
+								특별히 질문을 하지 않는다면 <font class="tacred">대화는 첫시간에 1 ~ 2분 정도에 불과</font>합니다. 흡연자의 흡연 패턴과 사고 방식을 분석하기 위한 질문들입니다.<br><br>
+								그러나 이조차도 불편하다면 테라피스트에게 미리 언급해주시기 바랍니다.
+								필요하다면 아무런 말없이 금연테라피에 참여하실 수 있습니다.
+							</div>
+						</td>
 					</tr>
 					<tr style="height: 20px;"></tr>
+					<tr style="cursor: pointer;" onclick="openSection('faq-16');">
+						<td style="width: 25px;"><font class="dp2 we700 tacred plus_faq-16">+</font></td>
+						<td class="cp1 we700 ln15 tale">보조제로 아무 효과가 없었어요. 중독을 끊는 것이 정말 가능할까요?</td>
+					</tr>
 					<tr>
-						<td style="width: 25px;"><font class="dp2 we700 tacred">+</font></td>
+						<td colspan="2">
+							<div id="faq-16" class="cp1 we300 ln18 tale pdglr30 tblack" style="display: none;">
+								<div class="h015"></div>
+								실제로 보조제는 위약 효과(Placebo Effect) 정도에 불과하다는 것이 학계의 정설입니다. 또한, 알렌카에서 이러한 보조제가 왜 금연에 '실질적인 방해 요소'가 되는지도 명확하게 이해하실 수 있을 것입니다.<br><br>
+								금연은 생각보다 대단히 교묘하고 복잡한 심리적 함정이기도 합니다. 정확한 접근법을 모르고 잘못된 방법을 쓴다면 아무 효과가 없는 것이 당연합니다. 알렌카에는 보조제나 약물로 실패했던 분들이 수도 없이 많이 참여합니다.
+							</div>
+						</td>
+					</tr>
+					<tr style="height: 20px;"></tr>
+					<tr style="cursor: pointer;" onclick="openSection('faq-17');">
+						<td style="width: 25px;"><font class="dp2 we700 tacred plus_faq-17">+</font></td>
 						<td class="cp1 we700 ln15 tale">저는 금단증상이 너무 심한 편입니다. 어렵지 않을까요?</td>
+					</tr>
+					<tr>
+						<td colspan="2">
+							<div id="faq-17" class="cp1 we300 ln18 tale pdglr30 tblack" style="display: none;">
+								<div class="h015"></div>
+								소위 전문가라 불리는 사람들은 그저 금단증상이 괴로워서 금연이 어려울 것이라고 엄포를 놓곤 합니다. 그러나 금단증상은 정확히 파악하지 못하면 그저 흡연자가 다시 담배를 피우기 위한 '훌륭한 핑계거리'에 불과합니다.<br><br>
+								알렌카에서는 금단증상을 신체적/정신적으로 분류하고 그 실체를 매우 정확하게 짚어냅니다. <font class="tacred">실제로 따져보면 신체의 고통보다는 정신적 고통이 훨씬 더 강하다는 것</font>을 알 수 있습니다.<br><br>
+								정신적 고통이 생기는 이유는 무언가 잘못된 학습이 있었기 때문입니다. 알렌카에서 이 부분을 바로 잡고나면 거짓말처럼 금단증상이 사라지는 경험을 합니다. 알렌카의 후기에 참여하신 분들, 카페에 후기글을 남겨주시는 분들 중 절대 다수가 금단증상이 사라졌다는 이야기를 합니다.
+							</div>
+						</td>
 					</tr>
 					<tr style="height: 20px;"></tr>
 				</table>
 			</div>			
 			<div class="col-md-6 pdg00">
 				<table width="100%">
-					<tr>
-						<td style="width: 25px;"><font class="dp2 we700 tacred">+</font></td>
+					<tr style="cursor: pointer;" onclick="openSection('faq-18');">
+						<td style="width: 25px;"><font class="dp2 we700 tacred plus_faq-18">+</font></td>
 						<td class="cp1 we700 ln15 tale">잠시라도 담배를 피우지 않으면 괴롭습니다. 5시간을 어떻게 견디나요?</td>
 					</tr>
-					<tr style="height: 20px;"></tr>
 					<tr>
-						<td style="width: 25px;"><font class="dp2 we700 tacred">+</font></td>
+						<td colspan="2">
+							<div id="faq-18" class="cp1 we300 ln18 tale pdglr30 tblack" style="display: none;">
+								<div class="h015"></div>
+								알렌카에는 하루 평균 4갑 이상을 피우는 골초 분들도 많이 참여하십니다. 이런 분들은 실제로 한시간 이상 담배없이 무언가 집중하는 것을 무척 어려워합니다.<br><br>
+								알렌카에서는 금연테라피를 진행하는 <font class="tacred">5시간 내내 흡연할 수 있도록 쉬는 시간(Smoking Break)을 제공</font>합니다. 참여하는 내내 편안하게 흡연하시고 마지막 담배를 버리는 그 순간부터 비흡연자의 삶을 사시면 됩니다.
+							</div>
+						</td>
+					</tr>
+					<tr style="height: 20px;"></tr>
+					<tr style="cursor: pointer;" onclick="openSection('faq-19');">
+						<td style="width: 25px;"><font class="dp2 we700 tacred plus_faq-19">+</font></td>
 						<td class="cp1 we700 ln15 tale">기억력이 안 좋습니다. 강의 내용을 모두 잊을까봐 걱정됩니다.</td>
 					</tr>
-					<tr style="height: 20px;"></tr>
 					<tr>
-						<td style="width: 25px;"><font class="dp2 we700 tacred">+</font></td>
-						<td class="cp1 we700 ln15 tale">30년 골초입니다. 만성화되어 알렌카가 통하지 않을까봐 걱정입니다.</td>
+						<td colspan="2">
+							<div id="faq-19" class="cp1 we300 ln18 tale pdglr30 tblack" style="display: none;">
+								<div class="h015"></div>
+								금연테라피는 최면을 포함하여 약 5시간 30분 정도의 시간이 소요됩니다. 이 모든 내용을 암기하는 것은 테라피스트와 같은 전문가의 영역입니다. 알렌카는 결코 암기에 의해 작동하는 것이 아닙니다.<br><br>
+								금연테라피는 철저하게 설득 → 이해 → 수용의 과정을 거칩니다. 모든 내용을 정확하게 받아들였다면, <font class="tacred">실제 테라피 내용의 대부분을 기억하지 못해도 크게 문제되지 않습니다.</font>
+							</div>
+						</td>
 					</tr>
 					<tr style="height: 20px;"></tr>
+					<tr style="cursor: pointer;" onclick="openSection('faq-20');">
+						<td style="width: 25px;"><font class="dp2 we700 tacred plus_faq-20">+</font></td>
+						<td class="cp1 we700 ln15 tale">30년 골초입니다. 만성화되어 알렌카가 통하지 않을까봐 걱정입니다.</td>
+					</tr>
 					<tr>
-						<td style="width: 25px;"><font class="dp2 we700 tacred">+</font></td>
+						<td colspan="2">
+							<div id="faq-20" class="cp1 we300 ln18 tale pdglr30 tblack" style="display: none;">
+								<div class="h015"></div>
+								알렌카에는 이제 막 담배를 시작한 15세 학생부터, 50년 이상을 흡연한 80대의 연로하신 분들까지 다양한 연령의 스펙트럼을 보유하고 있습니다.<br><br>
+								30년을 피웠다고해서 3년을 피운 사람보다 무언가 특별할 이유는 전혀 없습니다. 다만 보다 실패 경험이 많아 자신감이 떨어진 정도에 불과하다는 것이지요.<br><br>
+								애당초 금연을 결심하는 흡연자의 흡연 기간이 15년 이상이라는 것을 생각해보면, 알렌카에 참여하는 대다수 분들은 꽤 오래 피운 장기 흡연자들이라고 볼 수 있습니다. 전혀 문제되지 않습니다.
+							</div>
+						</td>
+					</tr>
+					<tr style="height: 20px;"></tr>
+					<tr style="cursor: pointer;" onclick="openSection('faq-21');">
+						<td style="width: 25px;"><font class="dp2 we700 tacred plus_faq-21">+</font></td>
 						<td class="cp1 we700 ln15 tale">하루 평균 4~5갑을 피우는 골초입니다. 정말 가능한가요?</td>
+					</tr>
+					<tr>
+						<td colspan="2">
+							<div id="faq-21" class="cp1 we300 ln18 tale pdglr30 tblack" style="display: none;">
+								<div class="h015"></div>
+								알렌카에서는 골초보다 하루에 1대 정도를 피우는 캐주얼 스모커(Casual Smoker)를 더 까다롭게 받아들입니다.<br><br>
+								골초는 비교적 흡연의 본질에 대해서 정확히 알고 있기 때문에, 테라피스트가 하는 모든 이야기에 대부분 공감하고 쉽게 변화합니다. 또한, 알렌카로 담배를 끊은 뒤의 긍정적 변화도 훨씬 극적으로 일어나기 때문에 여러면에서 긍정적입니다.<br><br>
+								단 하나의 문제는 대개 골초는 장기 흡연자고, 담배없는 인생을 상상조차 못하고 살아온 경우가 많아서 두려움이 많고 자신에 대한 의심도 많다는 점입니다. 골초 분들에게 알렌카가 드릴 수 있는 말은 단 하나입니다.<br><br>
+								<font class="tacred">실패하면 환불받으면 그만이라는 가벼운 마음으로 참여하십시오.<br>결코 가볍지 않은 결과를 드릴 것입니다.</font>
+							</div>
+						</td>
 					</tr>
 					<tr style="height: 20px;"></tr>
 				</table>				
@@ -257,25 +393,64 @@
 			<div class="h020"></div>
 			<div class="col-md-6 pdg00">
 				<table width="100%">
-					<tr>
-						<td style="width: 25px;"><font class="dp2 we700 tacred">+</font></td>
-						<td class="cp1 we700 ln15 tale">비용은 1차, 2차, 3차 각각 따로 지불하나요?</td>
+					<tr style="cursor: pointer;" onclick="openSection('faq-22');">
+						<td style="width: 25px;"><font class="dp2 we700 tacred plus_faq-22">+</font></td>
+						<td class="cp1 we700 ln15 tale">비용이 얼마지요? 1차, 2차, 3차 각각 따로 지불하나요?</td>
 					</tr>
-					<tr style="height: 20px;"></tr>
 					<tr>
-						<td style="width: 25px;"><font class="dp2 we700 tacred">+</font></td>
+						<td colspan="2">
+							<div id="faq-22" class="cp1 we300 ln18 tale pdglr30 tblack" style="display: none;">
+								<div class="h015"></div>
+								2018년 9월 현재 알렌카의 금연테라피는 398,000원으로 진행되고 있으며, 이는 대략 3개월치 담뱃값에 해당합니다.<br><br>
+								<font class="tacred">1갑(4,500원) x 90일 = 405,000원</font><br><br>
+								이 비용은 1차, 2차, 3차 모든 테라피와 환불보증제도까지 포함되어 있습니다. 따라서 알렌카에 참여하는 비용은 다음과 같이 정리할 수 있습니다.<br><br>
+								<font class="tacred"><b>알렌카 비용</b> = <br>3개월치 담뱃값 + 심화테라피 + 환불보증제도 + 온라인 보강 컨텐츠</font><br><br>
+								알렌카에 참여하는 모든 흡연자는 결코 아무것도 손해보지 않습니다.<br><br>
+							</div>
+						</td>
+					</tr>					
+					<tr style="height: 20px;"></tr>
+					<tr style="cursor: pointer;" onclick="openSection('faq-23');">
+						<td style="width: 25px;"><font class="dp2 we700 tacred plus_faq-23">+</font></td>
 						<td class="cp1 we700 ln15 tale">환불보증기간이 지난 후에는 다시 돈을 내야하나요?</td>
 					</tr>
-					<tr style="height: 20px;"></tr>
 					<tr>
-						<td style="width: 25px;"><font class="dp2 we700 tacred">+</font></td>
+						<td colspan="2">
+							<div id="faq-23" class="cp1 we300 ln18 tale pdglr30 tblack" style="display: none;">
+								<div class="h015"></div>
+								대다수 흡연자는 1차, 2차, 3차를 참여하는 동안 행복한 비흡연자가 됩니다. 그러나 실패 이후에도 환불보다는 재참여를 원하는 분들이 많습니다. <br><br>
+								<font class="tacred">이런 경우 심화테라피(2차, 3차)는 1년 동안 비용에 제한없이 참여</font>할 수 있으며, 1차의 경우 담당 테라피스트의 판단 하에 반드시 필요할 경우 참여가 가능합니다.<br><br>
+							</div>
+						</td>
+					</tr>					
+					<tr style="height: 20px;"></tr>
+					<tr style="cursor: pointer;" onclick="openSection('faq-24');">
+						<td style="width: 25px;"><font class="dp2 we700 tacred plus_faq-24">+</font></td>
 						<td class="cp1 we700 ln15 tale">카드 결제가 가능한가요?</td>
 					</tr>
-					<tr style="height: 20px;"></tr>
 					<tr>
-						<td style="width: 25px;"><font class="dp2 we700 tacred">+</font></td>
+						<td colspan="2">
+							<div id="faq-24" class="cp1 we300 ln18 tale pdglr30 tblack" style="display: none;">
+								<div class="h015"></div>
+								알렌카는 결제 PG 모듈로 이니시스를 이용하고 있으며, 주요 카드사를 모두 이용할 수 있습니다. 예약페이지에서 카드 결제가 가능합니다.
+							</div>
+						</td>
+					</tr>					
+					<tr style="height: 20px;"></tr>
+					<tr style="cursor: pointer;" onclick="openSection('faq-25');">
+						<td style="width: 25px;"><font class="dp2 we700 tacred plus_faq-25">+</font></td>
 						<td class="cp1 we700 ln15 tale">비용이 너무 부담됩니다. 할인 정책은 따로 없나요?</td>
 					</tr>
+					<tr>
+						<td colspan="2">
+							<div id="faq-25" class="cp1 we300 ln18 tale pdglr30 tblack" style="display: none;">
+								<div class="h015"></div>
+								알렌카의 비용은 3개월치 담뱃값으로 합리적인 근거로 책정되었습니다. 또한, 카드로 할부 결제가 가능하므로 일시불에 대한 부담도 줄일 수 있습니다.<br><br>
+								그럼에도 불구하고 비용에 민감한 분들이 많기 때문에, 웨비나 할인, 지인 추천에 의한 할인 등 다양한 프로모션을 준비하고 있습니다.<br><br>
+								그러나 흡연을 하고 있는 분이라면, 비용이 '진짜 문제'인지 한 번 돌아보시기 바랍니다. 알렌카에 참여를 미루기 위한 '구실'로 쓰이고 있지는 않은지요.
+							</div>
+						</td>
+					</tr>					
 					<tr style="height: 20px;"></tr>
 				</table>
 			</div>			
