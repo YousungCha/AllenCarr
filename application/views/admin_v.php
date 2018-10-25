666eaf4733b8980b21845840bcef459ad584c020
YousungCha <yscha08@gmail.com>
2018-10-25 15:28:01 +0900

    add : admin

--- application/views/admin_v.php
+++ application/views/admin_v.php
@@ -93,115 +93,199 @@
 
 			<div class="container-fluid bwhite pdg15">
 				<div class="h030"></div>
-				<p class="dp2 we700 tsilver">신청자 관리</p>
+				<p class="dp2 we700 tsilver">신청자 관리 1차</p>
 				<div class="h030"></div>
 				<table width="100%">
-					<tr class="bacnavy twhite we200 tace" style="height:40px;">
-						<td class="cp3">이름</td>
-						<td class="cp3">종류</td>
-						<td class="cp3">인원</td>
-						<td class="cp3">날짜</td>
-						<td class="cp3">상태</td>
-						<td class="cp3">결제방식</td>
-					</tr>
-
 					<?php foreach ($schedule as $sch_row) : ?>
-						<?php if ($sch_row['count'] == 1) : ?>
-						<tr>
-							<td colspan="6" class="bacred2 twhite tace pdg15">
+						<?php if ($sch_row['count'] == 1 && $sch_row['status'] != "hidden") : ?>
+						<tr onclick="openDate(<?=$sch_row['no']?>)" style="border-bottom: 1px solid silver;">
+							<td colspan="6" class="bacred2 twhite tace pdg15">								
 								<?=date("Y-m-d (D)",human_to_unix($sch_row['sdate']))?> : 
+								<font class="we200">
+								<?php 
+									$sql = "select no from session where date_1='".$sch_row['sdate']."' and status='1OK'";
+									$result = $this->db->query($sql);
+									echo $result->num_rows();
+								 ?>
+								 /
+								 <?php 
+									$sql = "select no from session where date_1='".$sch_row['sdate']."' and status='wait'";
+									$result = $this->db->query($sql);
+									echo $result->num_rows();
+								 ?>								 
+								</font>
+								<font class="we200 cp4 lt000 torange">(<?=$sch_row['status']?>)</font>
 							</td>
 						</tr>
-						<?php endif ?>					
-						<?php foreach($session as $row) :?>
-							<?php if ($row['date_1'] == $sch_row['sdate']) : ?>
-							<tr style="height:15px;"></tr>
-							<tr onclick="openMember(<?=$row['no']?>);">
-								<td class="cp3 we200 tace"><?=$row['name']?></td>						
-								<td class="cp3 we200 tace"><?=$row['class']?></td>
-								<td class="cp3 we200 tace"><?=$row['amount']?></td>
-								<td class="cp3 we200 tace"><?=mdate("%Y-%m-%d %g%A",human_to_unix($row['date_1']))?></td>
-								<td class="cp3 we200 tace"><?=$row['status']?></td>
-								<td class="cp3 we200 tace"><?=$row['payment']?></td>
-							</tr>
-							<tr style="height:15px;"></tr>
-							<tr style="height: 1px; background-color: #eeeeee;"><td colspan="6"></td></tr>							
-							<tr>
-								<td colspan="6">
-									<div class="container-fluid bwhite1" id="<?=$row['no']?>" style="display: none;">
-										<div class="h030"></div>
-											<center>
-											<?php 
-												$attributes = array('name' => 'sessionMemberChange', 'method' => 'post');
-												echo form_open(site_url('AdminSystem/btnChangeSessionMember'),$attributes); 
-											?>					
-												<input type="hidden" name="no" value="<?=$row['no']?>">						
-												<table>
-													<tr>
-														<td width="100">이름</td>
-														<td><input type="text" name="name" value="<?=$row['name']?>" style="width:100%;"></td>
-													</tr>
-													<tr style="height: 10px;"></tr>
-													<tr>
-														<td>이메일</td>
-														<td><input type="text" name="email" value="<?=$row['email']?>" style="width:100%;"></td>
-													</tr>
-													<tr style="height: 10px;"></tr>
-													<tr>
-														<td>전화번호</td>
-														<td><input type="text" name="phone" value="<?=$row['phone']?>" style="width:100%;"></td>
-													</tr>	
-													<tr style="height: 10px;"></tr>
-													<tr>
-														<td>날짜</td>
-														<td><input type="text" name="date_1" value="<?=$row['date_1']?>" style="width:100%;"></td>
-													</tr>	
-													<tr style="height: 10px;"></tr>
-													<tr>
-														<td>MBG</td>
-														<td><input type="text" name="mbg" value="<?=$row['mbg']?>" style="width:100%;"></td>
-													</tr>	
-													<tr style="height: 10px;"></tr>
-													<tr>
-														<td>상태</td>
-														<td>
-															<?=$row['status']?>&nbsp;
-															<select name="status" style="width:60%;">
-																<option value="wait">wait</option>
-																<option value="1OK">1OK</option>
-																<option value="2WAIT">2WAIT</option>
-																<option value="2OK">2OK</option>
-																<option value="3WAIT">3WAIT</option>
-																<option value="3OK">3OK</option>
-															</select>
-														</td>
-													</tr>
-													<tr style="height: 10px;"></tr>
-													<tr>
-														<td colspan="2">
-															<select name="delete" style="width:100%;">
-																<option value="no">상태유지</option>
-																<option value="yes">제거</option>
-															</select>														
-														</td>
-													</tr>												
-													<tr style="height: 10px;"></tr>
-													<tr>
-														<td colspan="2">
-															<button class="btn-general bacred twhite" type="submit" style="width:100%;">수정하기</button>
-														</td>
-													</tr>
-												</table>
-											</form>
-											</center>
-										<div class="h030"></div>
-									</div>
-								</td>
-							</tr>
-							<?php endif ?>							
-						<?php endforeach ?>
+						<?php endif ?>		
+
+						<?php if ($sch_row['count'] == 1) : ?>
+						<tr style="display: none;" id="_<?=$sch_row['no']?>">
+							<td>
+								<table width="100%">
+									<tr class="bacnavy twhite we200 tace" style="height:40px;">
+										<td class="cp3">이름</td>
+										<td class="cp3">종류</td>
+										<td class="cp3">인원</td>
+										<td class="cp3">날짜</td>
+										<td class="cp3">상태</td>
+										<td class="cp3">결제방식</td>
+									</tr>									
+									<?php foreach($session as $row) :?>
+										<?php if ($row['date_1'] == $sch_row['sdate']) : ?>
+										<tr style="height:15px;"></tr>
+										<tr onclick="openMember(<?=$row['no']?>);">
+											<td class="cp3 we200 tace"><?=$row['name']?></td>						
+											<td class="cp3 we200 tace"><?=$row['class']?></td>
+											<td class="cp3 we200 tace"><?=$row['amount']?></td>
+											<td class="cp3 we200 tace"><?=mdate("%Y-%m-%d %g%A",human_to_unix($row['date_1']))?></td>
+											<td class="cp3 we200 tace"><?=$row['status']?></td>
+											<td class="cp3 we200 tace"><?=$row['payment']?></td>
+										</tr>
+										<tr style="height:15px;"></tr>
+										<tr style="height: 1px; background-color: #eeeeee;"><td colspan="6"></td></tr>							
+										<tr>
+											<td colspan="6">
+												<div class="container-fluid bwhite1" id="<?=$row['no']?>" style="display: none;">
+													<div class="h030"></div>
+														<center>
+														<?php 
+															$attributes = array('name' => 'sessionMemberChange', 'method' => 'post');
+															echo form_open(site_url('AdminSystem/btnChangeSessionMember'),$attributes); 
+														?>					
+															<input type="hidden" name="no" value="<?=$row['no']?>">						
+															<table>
+																<tr>
+																	<td width="100">이름</td>
+																	<td><input type="text" name="name" value="<?=$row['name']?>" style="width:100%;"></td>
+																</tr>
+																<tr style="height: 10px;"></tr>
+																<tr>
+																	<td>이메일</td>
+																	<td><input type="text" name="email" value="<?=$row['email']?>" style="width:100%;"></td>
+																</tr>
+																<tr style="height: 10px;"></tr>
+																<tr>
+																	<td>전화번호</td>
+																	<td><input type="text" name="phone" value="<?=$row['phone']?>" style="width:100%;"></td>
+																</tr>	
+																<tr style="height: 10px;"></tr>
+																<tr>
+																	<td>날짜</td>
+																	<td><input type="text" name="date_1" value="<?=$row['date_1']?>" style="width:100%;"></td>
+																</tr>	
+																<tr style="height: 10px;"></tr>
+																<tr>
+																	<td>MBG</td>
+																	<td><input type="text" name="mbg" value="<?=$row['mbg']?>" style="width:100%;"></td>
+																</tr>	
+																<tr style="height: 10px;"></tr>
+																<tr>
+																	<td>상태</td>
+																	<td>
+																		<?=$row['status']?>&nbsp;
+																		<select name="status" style="width:60%;">
+																			<option value="wait">wait</option>
+																			<option value="1OK">1OK</option>
+																			<option value="2WAIT">2WAIT</option>
+																			<option value="2OK">2OK</option>
+																			<option value="3WAIT">3WAIT</option>
+																			<option value="3OK">3OK</option>
+																		</select>
+																	</td>
+																</tr>
+																<tr style="height: 10px;"></tr>
+																<tr>
+																	<td colspan="2">
+																		<select name="delete" style="width:100%;">
+																			<option value="no">상태유지</option>
+																			<option value="yes">제거</option>
+																		</select>														
+																	</td>
+																</tr>												
+																<tr style="height: 10px;"></tr>
+																<tr>
+																	<td colspan="2">
+																		<button class="btn-general bacred twhite" type="submit" style="width:100%;">수정하기</button>
+																	</td>
+																</tr>
+															</table>
+														</form>
+														</center>
+													<div class="h030"></div>
+												</div>
+											</td>
+										</tr>
+										<?php endif ?>							
+									<?php endforeach ?>
+								</table>
+							</td>
+						</tr>
+						<?php endif ?>
 					<?php endforeach ?>
-				</table>				
+				</table>			
+
+
+				<div class="h030"></div>
+				<p class="dp2 we700 tsilver">신청자 관리 2차</p>
+				<div class="h030"></div>
+				<table width="100%">
+					<?php foreach ($schedule as $sch_row) : ?>
+						<?php if ($sch_row['count'] == 2 && $sch_row['status'] != "hidden") : ?>
+						<tr onclick="openDate(<?=$sch_row['no']?>)" style="border-bottom: 1px solid silver;">
+							<td colspan="6" class="bacred2 twhite tace pdg15">								
+								<?=date("Y-m-d (D)",human_to_unix($sch_row['sdate']))?> : 
+								<font class="we200">
+								<?php 
+									$sql = "select no from session where date_2='".$sch_row['sdate']."' and status='2OK'";
+									$result = $this->db->query($sql);
+									echo $result->num_rows();
+								 ?>
+								 /
+								 <?php 
+									$sql = "select no from session where date_2='".$sch_row['sdate']."' and status='wait'";
+									$result = $this->db->query($sql);
+									echo $result->num_rows();
+								 ?>								 
+								</font>
+								<font class="we200 cp4 lt000 torange">(<?=$sch_row['status']?>)</font>
+							</td>
+						</tr>
+						<?php endif ?>		
+
+						<?php if ($sch_row['count'] == 2) : ?>
+						<tr style="display: none;" id="_<?=$sch_row['no']?>">
+							<td>
+								<table width="100%">
+									<tr class="bacnavy twhite we200 tace" style="height:40px;">
+										<td class="cp3">이름</td>
+										<td class="cp3">종류</td>
+										<td class="cp3">1차 날짜</td>
+										<td class="cp3">2차 날짜</td>
+										<td class="cp3">상태</td>
+										<td class="cp3">결제방식</td>
+									</tr>									
+									<?php foreach($session as $row) :?>
+										<?php if ($row['date_2'] == $sch_row['sdate']) : ?>
+										<tr style="height:15px;"></tr>
+										<tr onclick="openMember(<?=$row['no']?>);">
+											<td class="cp3 we200 tace"><?=$row['name']?></td>						
+											<td class="cp3 we200 tace"><?=$row['class']?></td>
+											<td class="cp3 we200 tace"><?=mdate("%Y-%m-%d %g%A",human_to_unix($row['date_1']))?></td>
+											<td class="cp3 we200 tace"><?=mdate("%Y-%m-%d %g%A",human_to_unix($row['date_2']))?></td>
+											<td class="cp3 we200 tace"><?=$row['status']?></td>
+											<td class="cp3 we200 tace"><?=$row['payment']?></td>
+										</tr>
+										<tr style="height:15px;"></tr>
+										<tr style="height: 1px; background-color: #eeeeee;"><td colspan="6"></td></tr>							
+										<?php endif ?>							
+									<?php endforeach ?>
+								</table>
+							</td>
+						</tr>
+						<?php endif ?>
+					<?php endforeach ?>
+				</table>						
 				<div class="h030"></div>		
 			</div>								
 			<div class="h015"></div>
@@ -225,6 +309,16 @@
 	</div>
 
 	<script type="text/javascript">
+		function openDate(id)
+		{
+			data = "#_" + id;	
+			if ($(data).css('display') == 'none') {			
+				$(data).slideDown(300);
+			}	
+			else {
+				$(data).slideUp(300);
+			}		
+		}
 		function openMember(id)
 		{		
 			data = "#" + id;	
